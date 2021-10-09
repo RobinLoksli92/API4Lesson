abb5ee0b1986658747e7f52701f4d8cb7e3745d7
RobinLoksli92 <gleb11tiun@mail.ru>
2021-10-10 02:38:02 +0300

    Новые названия переменным

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,11 +1,11 @@
 import requests
-from split_expansion import split_the_extension
+from split_extension import split_extension
 from save_image import save_image_from_url
 
 
 def fetch_spacex_some_launch(images_path,spacex_launch_number):
-    url_for_some_launch = f'https://api.spacexdata.com/v3/launches/{spacex_launch_number}'
-    response = requests.get(url_for_some_launch)
+    some_launch_launch = f'https://api.spacexdata.com/v3/launches/{spacex_launch_number}'
+    response = requests.get(some_launch_launch)
     response.raise_for_status()
 
     rocket_info = response.json()
@@ -13,6 +13,6 @@ def fetch_spacex_some_launch(images_path,spacex_launch_number):
     launch_images = launch_links.get('flickr_images')
 
     for image_number, image_url in enumerate(launch_images):
-        expansion_of_file = split_the_extension(image_url)
-        filename = f'{images_path}/spacex{image_number}.{expansion_of_file}'
+        file_extension = split_extension(image_url)
+        filename = f'{images_path}/spacex{image_number}.{file_extension}'
         save_image_from_url(image_url, filename)
--- split_extension.py
+++ split_extension.py
@@ -2,9 +2,9 @@ from urllib.parse import urlparse, urlsplit
 import os
 
 
-def split_the_extension(url):
+def split_extension(url):
     parsed_url = urlsplit(url)
-    path_of_url = parsed_url.path
-    splited_file = os.path.splitext(path_of_url)
-    extension_of_file = splited_file[1]
-    return extension_of_file
+    url_path = parsed_url.path
+    splited_file = os.path.splitext(url_path)
+    file_extension = splited_file[1]
+    return file_extension
